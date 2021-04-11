921da625c099a750a8c3441232e1347ed6d18ad0
Tim La Roche <timlaroche@users.noreply.github.com>
2021-04-11 14:54:43 +0100

    command line interface

--- ultimate_training.py
+++ ultimate_training.py
@@ -1,12 +1,12 @@
 from setuptools import setup
 import gym
-import flappy_env
 from stable_baselines3 import PPO, DQN
 from stable_baselines3.common.vec_env import VecVideoRecorder, DummyVecEnv
 from stable_baselines3.common.monitor import Monitor
+import argparse
 
-def train_dqn(itr):
-	env = flappy_env.FlappyEnv(server=True)
+def train_dqn(itr = 0, timesteps = 1e7, use_dummy_video = True):
+	env = flappy_env.FlappyEnv(use_dummy_video)
 	env = Monitor(env, f"flappy_dqn_{itr}")
 	obs = env.reset()
 	model = DQN(
@@ -17,11 +17,11 @@ def train_dqn(itr):
 		buffer_size = 500000, 
 		learning_rate = 1e-5, 
 		tensorboard_log = f"./dqn_flappy_tensorboard_{itr}/")
-	model.learn(total_timesteps = 1e7)
+	model.learn(total_timesteps = timesteps)
 	model.save(f"dqn_flappy_{itr}")
 
-def train_ppo(itr):
-	env = flappy_env.FlappyEnv(server=True)
+def train_ppo(itr=0, timesteps=1e7, use_dummy_video = True):
+	env = flappy_env.FlappyEnv(use_dummy_video)
 	env = Monitor(env, f"flappy_ppo_{itr}")
 	obs = env.reset()
 	model = PPO(
@@ -30,7 +30,7 @@ def train_ppo(itr):
 		verbose=1, 
 		learning_rate=1e-5,
 		tensorboard_log = f"./ppo_flappy_tensorboard_{itr}/")
-	model.learn(total_timesteps=1e7)
+	model.learn(total_timesteps = timesteps)
 	model.save(f"ppo_flappy_{itr}")
 
 def ultimate_training():
@@ -40,4 +40,19 @@ def ultimate_training():
 		train_ppo(i)
 
 if __name__ == '__main__':
-    ultimate_training()
+	parser = argparse.ArgumentParser()
+	parser.add_argument(
+		"--algorithm", 
+		choices=['ppo', 'dqn', 'ultimate'],
+		help = "The RL algorithm to use: PPO, DQN, Ultimate (train both 3 times) })",
+		required = True
+		)
+	args = parser.parse_args()
+	import flappy_env # Import here so argparse loads faster
+	if args.algorithm == "ppo":
+		train_dqn(0) 
+	elif args.algorithm == "dqn":
+		train_ppo(0)
+	elif args.algorithm == "ultimate":
+		ultimate_training()
+
